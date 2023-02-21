24bd7fb22ca08d2a3177c6f674194afb0ab311be
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-21 14:04:25 +0530

    Commit

--- src/main/java/com/lti/demo/App.java
+++ src/main/java/com/lti/demo/App.java
@@ -1,12 +1,13 @@
 package com.lti.demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
-		ApplicationContext context;
+		ApplicationContext context = new ClassPathXmlApplicationContext();
 
 	}
 }
