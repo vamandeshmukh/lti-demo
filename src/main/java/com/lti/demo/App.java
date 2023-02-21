eaa475c354143bd0c52fd9dcffda3347b888ad35
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-21 14:18:12 +0530

    Commit

--- src/main/java/com/lti/demo/App.java
+++ src/main/java/com/lti/demo/App.java
@@ -1,13 +1,10 @@
 package com.lti.demo;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
-		ApplicationContext context = new ClassPathXmlApplicationContext();
+		Employee emp;
 
 	}
 }
---
+++ src/main/java/com/lti/demo/Employee.java
@@ -0,0 +1,14 @@
+package com.lti.demo;
+
+public class Employee {
+
+	int employeeId;
+	String firstName;
+	double salary;
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", salary=" + salary + "]";
+	}
+
+}
