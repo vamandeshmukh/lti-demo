afe3dddb9ee14f73297fcd66313787284f75e386
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-21 12:21:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lti-demo
+ Maven spring demo for LTI BLR trainees 20 Feb 2023
---
+++ src/main/java/com/lti/demo/App.java
@@ -0,0 +1,12 @@
+package com.lti.demo;
+
+import org.springframework.context.ApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		ApplicationContext context;
+
+	}
+}
